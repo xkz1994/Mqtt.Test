d69fbc71f2bef63dc390490f69d14341337e6066
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-11-26 13:44:12 +0800

    添加项目文件。

---
+++ Mqtt.Rpc.Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using MQTTnet.Client;
+using MQTTnet.Protocol;
+using MQTTnet;
+
+var mqttFactory = new MqttFactory();
+
+using var mqttClient = mqttFactory.CreateMqttClient();
+var mqttClientOptions = new MqttClientOptionsBuilder()
+    .WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("iot.nnmz.aacoptics.com", 1883)
+    .WithCredentials("admin", "public")
+    .WithTimeout(TimeSpan.FromSeconds(30))
+    .WithKeepAlivePeriod(TimeSpan.FromSeconds(20))
+    .Build();
+
+await mqttClient.ConnectAsync(mqttClientOptions);
+
+
+await mqttClient.SubscribeAsync("MQTTnet.RPC/+/ping");
+
+mqttClient.ApplicationMessageReceivedAsync += eventArgs =>
+{
+    Console.WriteLine(
+        $"{eventArgs.ApplicationMessage.Topic} => {eventArgs.ApplicationMessage.ConvertPayloadToString()}");
+    if (eventArgs.ApplicationMessage.Topic.StartsWith("MQTTnet.RPC/"))
+    {
+        // ReSharper disable once AccessToDisposedClosure
+        return mqttClient.PublishAsync(new MqttApplicationMessage
+        {
+            Topic = $"{eventArgs.ApplicationMessage.Topic}/response",
+            QualityOfServiceLevel = MqttQualityOfServiceLevel.AtMostOnce,
+            Payload = eventArgs.ApplicationMessage.Payload
+        });
+    }
+
+    return Task.CompletedTask;
+};
+
+Console.ReadKey();
---
+++ Mqtt.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet.Client;
+using MQTTnet.Protocol;
+using MQTTnet;
+using MQTTnet.Extensions.Rpc;
+using Net.Utilities.Object.String.Constant;
+using Net.Utilities.Struct.Byte;
+
+var mqttFactory = new MqttFactory();
+
+using var mqttClient = mqttFactory.CreateMqttClient();
+var mqttClientOptions = new MqttClientOptionsBuilder()
+    .WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("iot.nnmz.aacoptics.com", 1883)
+    .WithCredentials("admin", "public")
+    .WithTimeout(TimeSpan.FromSeconds(30))
+    .WithKeepAlivePeriod(TimeSpan.FromSeconds(20))
+    .Build();
+
+await mqttClient.ConnectAsync(mqttClientOptions);
+
+using var mqttRpcClient = mqttFactory.CreateMqttRpcClient(mqttClient);
+
+while (true)
+{
+    var randomGuidString = EnvironmentHelper.RandomGuidString;
+    var executeAsync =
+        await mqttRpcClient.ExecuteAsync(TimeSpan.FromSeconds(2), "ping", randomGuidString,
+            MqttQualityOfServiceLevel.AtMostOnce);
+    var bytes2Utf8String = ByteCastHelper.Bytes2Utf8String(executeAsync);
+    Console.WriteLine(bytes2Utf8String);
+    if (bytes2Utf8String != randomGuidString)
+    {
+        throw new Exception("测试失败");
+    }
+}
